158390793ae6b11b81b1fe7d686e897d459a7962
atrox3d <atrox3d@gmail.com>
2022-09-06 12:03:03 +0200

    74. Building the CLI Interface - logic

--- main.py
+++ main.py
@@ -1,9 +1,61 @@
-from canvas import canvas
+from canvas import Canvas
 from shapes import Rectangle, Square
 
-r1 = Rectangle(row=1, column=6, height=7, width=10, color=(100, 200, 125))
-r1.draw(canvas)
-s1 = Square(row=1, column=3, side=3, color=(0, 100, 222))
-s1.draw(canvas)
-canvas.make('images/test.png')
+# r1 = Rectangle(row=1, column=6, height=7, width=10, color=(100, 200, 125))
+# r1.draw(canvas)
+# s1 = Square(row=1, column=3, side=3, color=(0, 100, 222))
+# s1.draw(canvas)
+# canvas.make('images/test.png')
+
+canvas_width = int(input('Enter canvas width: '))
+canvas_heigth = int(input('Enter canvas heigth: '))
+
+colors = {
+    'white': (255, 255, 255),
+    'black': (0, 0, 0)
+}
+while (canvas_color := input("Enter canvas color (white or black): ").lower()) not in colors.keys():
+    pass
+canvas = Canvas(
+    height=canvas_heigth,
+    width=canvas_width,
+    color=colors[canvas_color]
+)
+choices = ['rectangle', 'square']
+while True:
+    while (
+            shape_type := input(
+                'What would you like to draw? '
+                '(square, rectangle, quit exits): '
+            ).lower()
+    ) in choices:
+        row = int(input(f'Enter row for {shape_type}: '))
+        column = int(input(f'Enter column for {shape_type}: '))
+
+        if shape_type == "rectangle":
+            width = int(input(f'Enter width of {shape_type}: '))
+            height = int(input(f'Enter height of {shape_type}: '))
+        else:
+            side = int(input(f'Enter side of {shape_type}: '))
+
+        while (red := int(input(f'How much red (0-255) for {shape_type}?: '))) not in range(0, 256):
+            pass
+
+        while (green := int(input(f'How much green (0-255) for {shape_type}?: '))) not in range(0, 256):
+            pass
+
+        while (blue := int(input(f'How much blue (0-255) for {shape_type}?: '))) not in range(0, 256):
+            pass
+
+        if shape_type == 'rectangle':
+            r1 = Rectangle(row=row, column=column, height=height, width=width, color=(red, green, blue))
+            r1.draw(canvas)
+        else:
+            s1 = Square(row=row, column=column, side=side, color=(red, green, blue))
+            s1.draw(canvas)
+    else:
+        if shape_type == 'quit':
+            break
+
+canvas.make('images/shapes.png')
 
