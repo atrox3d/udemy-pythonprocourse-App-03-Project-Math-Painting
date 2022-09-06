dc47d1c308d8fef273689cc70783c4c50139718e
atrox3d <atrox3d@gmail.com>
2022-09-06 06:25:09 +0200

    73. Implementing the Methods

--- main.py
+++ main.py
@@ -1,33 +1,55 @@
+from PIL import Image
+import numpy as np
+
+
 class Canvas:
 
-    def __init__(self, a, b, color):
-        self.a = a
-        self.b = b
+    def __init__(self, height, width, color):
+        self.height = height
+        self.width = width
         self.color = color
 
-    def make(self):
-        pass
+        self.data = np.zeros((self.height, self.width, 3), dtype=np.uint8)
+        self.data[:] = self.color
+
+    def make(self, imagepath):
+        img = Image.fromarray(self.data, 'RGB')
+        img.save(imagepath)
 
 
 class Rectangle:
 
-    def __init__(self, x, y, a, b, color):
-        self.x = x
-        self.y = y
-        self.a = a
-        self.b = b
+    def __init__(self, row, column, height, width, color):
+        self.row = row
+        self.column = column
+        self.height = height
+        self.width = width
         self.color = color
 
     def draw(self, canvas):
-        pass
+        canvas.data[self.row: self.row + self.height, self.column:self.column + self.width] = self.color
 
 
 class Square:
-    def __init__(self, x, y, a, color):
-        self.x = x
-        self.y = y
-        self.a = a
+    def __init__(self, row, column, side, color):
+        self.row = row
+        self.column = column
+        self.side = side
         self.color = color
 
     def draw(self, canvas):
-        pass
+        canvas.data[self.row: self.row + self.side, self.column:self.column + self.side] = self.color
+
+
+canvas = Canvas(
+    height=20,
+    width=30,
+    color=(255, 255, 255)
+)
+
+r1 = Rectangle(row=1, column=6, height=7, width=10, color=(100, 200, 125))
+r1.draw(canvas)
+s1 = Square(row=1, column=3, side=3, color=(0, 100, 222))
+s1.draw(canvas)
+canvas.make('test.png')
+
